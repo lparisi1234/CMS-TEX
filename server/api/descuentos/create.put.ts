3d6ccb2097426f230565be4b8598e5baafe87237
Francis <joafransanmer@gmail.com>
2025-07-21 09:40:28 -0300

    add crud endpoints "descuentos"

---
+++ server/api/descuentos/create.put.ts
@@ -0,0 +1,45 @@
+import { pool } from '../../db'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const {
+      nombre,
+      img,
+      estado,
+      codigo_newton,
+      
+    } = await readBody(event)
+
+    if (
+      nombre === undefined ||
+      img === undefined ||
+      estado === undefined ||
+      codigo_newton === undefined 
+    ) {
+      return { success: false, message: 'Faltan campos requeridos' }
+    }
+
+    const query = `
+      INSERT INTO "Descuentos" (
+        nombre,
+        img,
+        estado,
+        codigo_newton
+      ) VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `;
+
+    const values = [
+      nombre,
+      img,
+      estado,
+      codigo_newton,
+    ];
+
+    const result = await pool.query(query, values)
+    return { success: true, message: 'Descuento creado correctamente', blog: result.rows[0] }
+  } catch (error) {
+    console.error('Error creando Descuento:', error)
+    return { success: false, message: 'Error creando Descuento' }
+  }
+})
