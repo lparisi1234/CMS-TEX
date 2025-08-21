59f2b295e9c62e11ad7d8d8b81fb1339cb559ea2
Francis <joafransanmer@gmail.com>
2025-08-21 20:32:59 -0300

    fix: db

--- server/db.js
+++ server/db.js
@@ -7,9 +7,9 @@ const region = 'us-east-1';
 const secretsClient = new SecretsManagerClient({ region });
 
 async function getDbPool() {
-  // const command = new GetSecretValueCommand({ SecretId: secretName });
-  // const response = await secretsClient.send(command);
-  // const secret = JSON.parse(response.SecretString);
+  const command = new GetSecretValueCommand({ SecretId: secretName });
+  const response = await secretsClient.send(command);
+  const secret = JSON.parse(response.SecretString);
 
   const pool = new Pool({
     host: "tex2-dev.cluster-c0lq6suu44up.us-east-1.rds.amazonaws.com",
