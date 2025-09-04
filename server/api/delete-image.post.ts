8e1b82961d4b846dd303dc13ea8c15724e88a396
Nicolas Rosso <nicolas.rossofor@gmail.com>
2025-09-04 18:41:39 -0300

    Fix image delete

--- server/api/delete-image.post.ts
+++ server/api/delete-image.post.ts
@@ -21,25 +21,40 @@ export default defineEventHandler(async (event) => {
       })
     }
 
-    // Extraer la ruta S3 de la URL
-    // URL: https://tex2-static-images-prd.s3.amazonaws.com/carpeta/archivo.jpg
-    // Ruta S3: s3://tex2-static-images-prd/carpeta/archivo.jpg
-    const urlParts = imageUrl.split('.s3.us-east-1.amazonaws.com/')
-    if (urlParts.length !== 2) {
+    // Extraer la ruta S3 de la URL (maneja URLs con y sin región)
+    // URL con región: https://tex2-static-images-prd.s3.us-east-1.amazonaws.com/carpeta/archivo.jpg
+    // URL sin región: https://tex2-static-images-prd.s3.amazonaws.com/carpeta/archivo.jpg
+    let bucketName, s3Key
+
+    // Patrón para URLs con región: bucket.s3.region.amazonaws.com
+    const regionalPattern = /https:\/\/([^.]+)\.s3\.([^.]+)\.amazonaws\.com\/(.+)/
+    // Patrón para URLs sin región: bucket.s3.amazonaws.com  
+    const standardPattern = /https:\/\/([^.]+)\.s3\.amazonaws\.com\/(.+)/
+
+    const regionalMatch = imageUrl.match(regionalPattern)
+    const standardMatch = imageUrl.match(standardPattern)
+
+    if (regionalMatch) {
+      bucketName = regionalMatch[1]
+      s3Key = regionalMatch[3]
+    } else if (standardMatch) {
+      bucketName = standardMatch[1] 
+      s3Key = standardMatch[2]
+    } else {
       throw createError({
         statusCode: 400,
         statusMessage: 'URL de imagen inválida'
       })
     }
 
-    const bucketName = 'tex2-static-images-prd'
-    const s3Key = urlParts[1]
     const s3Path = `s3://${bucketName}/${s3Key}`
 
     // Comando AWS S3 RM
     const command = `aws s3 rm "${s3Path}"`
 
     console.log('Ejecutando comando:', command)
+    console.log('Bucket:', bucketName)
+    console.log('Key:', s3Key)
 
     // Ejecutar comando AWS CLI
     const { stdout, stderr } = await execAsync(command)
