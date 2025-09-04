2b8b478b06ce6c29ca350f30c47208328b71cbea
Nicolas Rosso <nicolas.rossofor@gmail.com>
2025-09-03 22:00:06 -0300

    test create img

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,37 +1,5 @@
 <template>
     <DefaultSection>
-        <HeadingH1>Home Test - Prueba ImageField</HeadingH1>
-        <div class="mt-8 max-w-md mx-auto">
-            <FormImageField 
-                id="test-image"
-                label="Subir imagen a S3"
-                @upload-complete="onUploadComplete"
-                @upload-error="onUploadError"
-            />
-            
-            <div v-if="uploadResult" class="mt-4 p-3 rounded-md" :class="uploadResult.success ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'">
-                {{ uploadResult.message }}
-            </div>
-        </div>
+        <HeadingH1>Home Test</HeadingH1>
     </DefaultSection>
 </template>
-
-<script setup>
-import { ref } from "vue"
-
-const uploadResult = ref(null)
-
-const onUploadComplete = (s3Url) => {
-    uploadResult.value = {
-        success: true,
-        message: `Imagen subida exitosamente a: ${s3Url}`
-    }
-}
-
-const onUploadError = (error) => {
-    uploadResult.value = {
-        success: false,
-        message: `Error: ${error}`
-    }
-}
-</script>
