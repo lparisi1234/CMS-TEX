5a4d0166cd9690ea76ff9f2c2fb61ccac2a0e407
Lucas Popritkin <lucas.popri@gmail.com>
2025-10-08 13:20:01 -0300

    db

--- server/sql/create_tour_newton.sql
+++
@@ -1,7 +0,0 @@
--- Crear la tabla tour_newton
-CREATE TABLE IF NOT EXISTS tour_newton (
-    id SERIAL PRIMARY KEY,
-    newton_id VARCHAR(255) NOT NULL UNIQUE,
-    nombre VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
